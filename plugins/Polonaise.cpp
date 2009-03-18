b25e5a13f0d5660f608c1ad22f49387ded953c60
Simon Stiefel <simon@stiefels.net>
2009-03-18 11:04:42 +0100

    Added Polonaise plugin (copied from OneTurning)

---
+++ plugins/Polonaise.cpp
@@ -0,0 +1,164 @@
+// ----------------------------------------------------------------------------
+//
+//
+// OpenSteer -- Steering Behaviors for Autonomous Characters
+//
+// Copyright (c) 2002-2003, Sony Computer Entertainment America
+// Original author: Craig Reynolds <craig_reynolds@playstation.sony.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//
+// ----------------------------------------------------------------------------
+//
+//
+// One vehicle turning way: a (near) minimal OpenSteerDemo PlugIn
+//
+// 06-24-02 cwr: created 
+//
+//
+// ----------------------------------------------------------------------------
+
+
+#include <iomanip>
+#include <sstream>
+#include "OpenSteer/SimpleVehicle.h"
+#include "OpenSteer/OpenSteerDemo.h"
+
+
+using namespace OpenSteer;
+
+
+// ----------------------------------------------------------------------------
+
+
+class Polonaise : public SimpleVehicle
+{
+public:
+
+    // constructor
+    Polonaise () {reset ();}
+
+    // reset state
+    void reset (void)
+    {
+        SimpleVehicle::reset (); // reset the vehicle 
+        setSpeed (1.5f);         // speed along Forward direction.
+        setMaxForce (0.3f);      // steering force is clipped to this magnitude
+        setMaxSpeed (5);         // velocity is clipped to this magnitude
+        clearTrailHistory ();    // prevent long streaks due to teleportation 
+    }
+
+    // per frame simulation update
+    void update (const float currentTime, const float elapsedTime)
+    {
+        applySteeringForce (Vec3 (-2, 0, -3), elapsedTime);
+        annotationVelocityAcceleration ();
+        recordTrailVertex (currentTime, position());
+    }
+
+    // draw this character/vehicle into the scene
+    void draw (void)
+    {
+        drawBasic2dCircularVehicle (*this, gGray50);
+        drawTrail ();
+    }
+};
+
+
+// ----------------------------------------------------------------------------
+// PlugIn for OpenSteerDemo
+
+
+class PolonaisePlugIn : public PlugIn
+{
+public:
+    
+    const char* name (void) {return "Polonaise";}
+
+    float selectionOrderSortKey (void) {return 0.06f;}
+
+    // be more "nice" to avoid a compiler warning
+    virtual ~PolonaisePlugIn() {}
+
+    void open (void)
+    {
+        gPolonaise = new Polonaise;
+        OpenSteerDemo::selectedVehicle = gPolonaise;
+        theVehicle.push_back (gPolonaise);
+
+        // initialize camera
+        OpenSteerDemo::init2dCamera (*gPolonaise);
+        OpenSteerDemo::camera.setPosition (10,
+                                           OpenSteerDemo::camera2dElevation,
+                                           10);
+        OpenSteerDemo::camera.fixedPosition.set (40, 40, 40);
+    }
+
+    void update (const float currentTime, const float elapsedTime)
+    {
+        // update simulation of test vehicle
+        gPolonaise->update (currentTime, elapsedTime);
+    }
+
+    void redraw (const float currentTime, const float elapsedTime)
+    {
+        // draw test vehicle
+        gPolonaise->draw ();
+
+        // textual annotation (following the test vehicle's screen position)
+        std::ostringstream annote;
+        annote << std::setprecision (2) << std::setiosflags (std::ios::fixed);
+        annote << "      speed: " << gPolonaise->speed() << std::ends;
+        draw2dTextAt3dLocation (annote, gPolonaise->position(), gRed);
+        draw2dTextAt3dLocation (*"start", Vec3::zero, gGreen);
+
+        // update camera, tracking test vehicle
+        OpenSteerDemo::updateCamera (currentTime, elapsedTime, *gPolonaise);
+
+        // draw "ground plane"
+        OpenSteerDemo::gridUtility (gPolonaise->position());
+    }
+
+    void close (void)
+    {
+        theVehicle.clear ();
+        delete (gPolonaise);
+        gPolonaise = NULL;
+    }
+
+    void reset (void)
+    {
+        // reset vehicle
+        gPolonaise->reset ();
+    }
+
+    const AVGroup& allVehicles (void) {return (const AVGroup&) theVehicle;}
+
+    Polonaise* gPolonaise;
+    std::vector<Polonaise*> theVehicle; // for allVehicles
+};
+
+
+PolonaisePlugIn gPolonaisePlugIn;
+
+
+
+
+// ----------------------------------------------------------------------------
